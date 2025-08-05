d4f9b578a54bbe56e13ef9df9e70cbb607249c35
iotaylor <xyunpro@gmail.com>
2025-08-05 22:20:14 +0800

    Revert "优化代码"
    This reverts commit 81444cc83a59b12ffd584cb3081e134e73076327.

--- src/helpers/ipc/listeners-register.ts
+++ src/helpers/ipc/listeners-register.ts
@@ -3,7 +3,6 @@ import { registerHostListeners } from "./host-listeners";
 import { registerSettingsListeners } from "./settings-listeners";
 import { addThemeEventListeners } from "./theme/theme-listeners";
 import { addWindowEventListeners } from "./window/window-listeners";
-import { registerAppListeners } from "./app-listeners";
 
 // 添加全局IPC监听器
 function addGlobalListeners() {
@@ -36,7 +35,6 @@ function addGlobalListeners() {
   }
 
   // 注册其他全局监听器
-  registerAppListeners();
   addThemeEventListeners();
   registerHostListeners();
   registerSettingsListeners();
